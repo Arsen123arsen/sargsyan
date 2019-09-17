499b08ede2d4070c9b772fb7b90e1eec13a9dc85
Your Name <you@example.com>
2019-09-17 14:39:11 +0400

    First ommit

---
+++ Guard/Guard1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Guard1Config(AppConfig):
+    name = 'Guard1'
---
+++ Guard/Guard1/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^', views.index ,name='index'),
+    url(r'^home.aspx', views.index1 , name='index1')
+]
+
