206503e739a625b56ab9411445fab67111d0d740
Your Name <you@example.com>
2019-10-04 20:50:53 +0400

    Artak

--- Guard/Guard1/views.py
+++ Guard/Guard1/views.py
@@ -6,4 +6,4 @@ def index(request):
     return render(request,'guard1/index.html'),
 
 def index1(request):
-    return render(request,'Guard1/index.html'),
+    return render(request,'guard1/arsen.html'),
